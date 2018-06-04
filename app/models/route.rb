536a094abe5a569871b6f360c23da889b2c73e1e
kievbuy <kievbuy@gmail.com>
2018-06-04 19:14:23 +0000

    add views for routes

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,4 +2,6 @@ class Route < ActiveRecord::Base
     # has_and_belongs_to_many :railway_stations
     has_many :railway_stations_routes
     has_many :railway_stations, through: :railway_stations_routes
+    
+    validates :name, presence: true
 end
